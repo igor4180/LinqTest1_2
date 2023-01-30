e62a78813ec61dc5ac5b9b3dee68589f2cc8f57d
Игорь <@DESKTOP-B894K3L>
2023-01-30 14:09:20 +0300

    73

---
+++ LinqTest1/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTest1
+{
+    class Product
+    {
+        public string Name { set; get; }
+        public int Price { set; get; }
+        public string Manufacturer { set; get; }
+        public int Count { set; get; }
+        public override string ToString()
+        {
+            return String.Format("{0}  {1}  {2}  {3}",this.Name, this.Price,this.Manufacturer,this.Count);
+        }
+    }
+}
---
+++ LinqTest1/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LinqTest1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            LinqMethod3();
+        }
+
+        private static void LinqMethod1()
+        {
+            //массив - источник данных
+            string[] countries = { "Albania","UK","Lithuania","Andorra",
+                "Austria", "Latvia", "Liechtenstein", "Switzerland",
+                "Ireland", "Sweden","Italy", "France","Liechtenstein",
+                "Spain", "Turkey", "Germany", "Switzerland", "Monaco",
+                "Montenegro", "Norway", "Finland", "Turkey", "UK",
+                "Poland", "Portugal", "Lithuania", "Luxembourg"
+                };
+            //LINQ запрос
+            var result = (from c in countries
+                         where c.StartsWith("L")
+                         //where c.Length > 10
+                         orderby c.Length descending
+                         select c).Distinct();
+
+            Console.WriteLine("Начинаются на L:");
+            //продолжение работы с запросом
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.Write("Нажмите Enter для подтверждения");
+            Console.ReadLine();
+        }
+
+        private static void LinqMethod2()
+        {
+            //массив - источник данных
+            string[] countries = { "Albania","UK","Lithuania","Andorra",
+                "Austria", "Latvia", "Liechtenstein", "Switzerland",
+                "Ireland", "Sweden","Italy", "France","Liechtenstein",
+                "Spain", "Turkey", "Germany", "Switzerland", "Monaco",
+                "Montenegro", "Norway", "Finland", "Turkey", "UK",
+                "Poland", "Portugal", "Lithuania", "Luxembourg"
+                };
+            //LINQ запрос
+            var result = countries.OrderBy(c => c.Length).Where(c => c.StartsWith("L")).Distinct();
+
+            Console.WriteLine("Countries beginning with L:");
+            //продолжение работы с запросом
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\nAggregate functions:\n");
+            Console.WriteLine(result.Max());
+            Console.WriteLine(result.Min());
+            Console.WriteLine(result.Count());
+            //Console.WriteLine(result.Average());
+            //Console.WriteLine(result.Sum());
+            
+            Console.ReadLine();
+        }
+
+        delegate int del(int i);
+        static void TestLambda()
+        {
+            del myDelegate = (x) => x * x;
+            int sq = myDelegate(5); //sq = 25
+        }
+
+        private static void LinqMethod3()
+        {
+            //массив - источник данных
+            string[] countries = { "Albania","UK","Lithuania","Andorra",
+                "Austria", "Latvia", "Liechtenstein", "Switzerland",
+                "Ireland", "Sweden","Italy", "France","Liechtenstein",
+                "Spain", "Turkey", "Germany", "Switzerland", "Monaco",
+                "Montenegro", "Norway", "Finland", "Turkey", "UK",
+                "Poland", "Portugal", "Lithuania", "Luxembourg"
+                };
+
+            //List<Product> products = new List<Product>();
+            //for (int i= 0; i<100; i++)
+            //{
+            //    Thread.Sleep(5);
+            //    products.Add(new Product { Name = "Product" + (++i),
+            //        Price = (new Random()).Next(0,1000),
+            //        Manufacturer = countries[(new Random()).Next(0, countries.Length - 1)],
+            //        Count = (new Random()).Next(0, 10) });
+            //}
+            //Console.WriteLine("All products:");
+            //foreach (var item in products)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //LINQ запрос
+            //var result = from c in products
+            //             where c.Price<500
+            //             orderby c.Price 
+            //             select new { c.Name, c.Price };
+
+            //var result = products.Where(p => p.Price > 800)
+            //    .Select(p => new { p.Name, p.Price });
+
+            //Console.WriteLine("\nProducts with price greater than 800:");
+            ////продолжение работы с запросом
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("Total count:"+result.Count());
+            //Console.WriteLine("\nTop 5:\n");
+            //foreach (var item in result.Take(5))
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("\nAfter top 5:\n");
+            //foreach (var item in result.Skip(5))
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //var pr = products.FirstOrDefault(p => p.Price > 500 &&
+            //    p.Manufacturer.StartsWith("L"));
+            //Console.WriteLine("\nFirst:"+pr);
+            //Console.ReadLine();
+
+        }
+    }
+}
